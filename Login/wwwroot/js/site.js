4e3ee1056f415453a4e03770bfd1a8163c751aed
yusufali0 <yusufali.bekci@hotmail.com>
2024-04-25 17:53:45 +0300

    chat

--- Login/wwwroot/js/site.js
+++ Login/wwwroot/js/site.js
@@ -1,32 +1,32 @@
-﻿async function sendMessage() {
-    const message = document.getElementById("message").value;
-    const apiUrl = "/Home/SendMessage"; 
+﻿//async function sendMessage() {
+//    const message = document.getElementById("message").value;
+//    const apiUrl = "/Home/SendMessage"; 
 
-    const formData = new URLSearchParams();
-    formData.append("message", message);
+//    const formData = new URLSearchParams();
+//    formData.append("message", message);
 
-    try {
-        const response = await fetch(apiUrl, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
-            },
-            body: formData
-        });
+//    try {
+//        const response = await fetch(apiUrl, {
+//            method: "POST",
+//            headers: {
+//                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+//            },
+//            body: formData
+//        });
 
-        if (response.ok) {
-            const data = await response.text();
-            appendMessage(data, 'response');
-        } else {
-            const errorText = await response.text();
-            console.error(`API Error: ${errorText}`);
-            appendMessage(`Error: ${response.statusText}`, 'error');
-        }
-    } catch (error) {
-        console.error(`Fetch Error: ${error.message}`);
-        appendMessage(`Error: ${error.message}`, 'error');
-    }
-}
+//        if (response.ok) {
+//            const data = await response.text();
+//            appendMessage(data, 'response');
+//        } else {
+//            const errorText = await response.text();
+//            console.error(`API Error: ${errorText}`);
+//            appendMessage(`Error: ${response.statusText}`, 'error');
+//        }
+//    } catch (error) {
+//        console.error(`Fetch Error: ${error.message}`);
+//        appendMessage(`Error: ${error.message}`, 'error');
+//    }
+//}
 
 //document.addEventListener("DOMContentLoaded", function () {
 //    var userInput = document.getElementById('userInput');
